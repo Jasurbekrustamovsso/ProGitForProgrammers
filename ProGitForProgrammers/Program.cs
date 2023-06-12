3bdc2cd2434c8b0c040901fdc881799121aa02fa
Jaska <Jaska.95rjr@gmail.com>
2023-06-12 22:47:21 +0500

    aa

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This statemnet has been added now");
 Console.WriteLine("One more statement has been added to our program");
 
+=======
+﻿
+public class Calculator
+    {
+
+    }
+>>>>>>> Stashed changes
